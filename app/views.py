7a8861bddd56572ae5cafe6880282f1b76348324
游发 <chinayoufa1994@gmail.com>
2017-04-15 18:07:01 +0800

    支持添加数据

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from app.models import Todo
 
 
 @app.route('/')
@@ -9,3 +10,12 @@ def index():
     from app.models import Todo
     todos = Todo.objects.all()
     return render_template('index.html', todos=todos)
+
+
+@app.route('/add', methods=['POST', ])
+def add():
+    content = request.form.get('content')
+    if content:
+        todo = Todo(content=content)
+        todo.save()
+        return redirect(url_for('index'))
